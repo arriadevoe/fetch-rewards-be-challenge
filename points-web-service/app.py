5fd960e4f91c8a8d7b83a61a203c98a9a46c82cf
sapinspys <garri009@fiu.edu>
2021-02-21 00:21:31 -0500

    Merged transaction routes into single one

--- points-web-service/app.py
+++ points-web-service/app.py
@@ -6,20 +6,12 @@ app.debug = True
 data_store = []
 
 
-@app.route('/transaction', methods=['POST'])
-def add_transaction():
-    request_body = app.current_request.json_body
-    data_store.append(request_body)
-    return data_store
-
-
 @app.route('/transactions', methods=['POST'])
 def add_transaction():
     request_body = app.current_request.json_body
 
-    if type(request_body) == list:
-        for ts in request_body:
-            data_store.append(request_body)
+    for ts in request_body:
+        data_store.append(request_body)
     
     return data_store
 
