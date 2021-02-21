eb227d4954ccaa534047e10d7cff0e46356bb9a6
sapinspys <garri009@fiu.edu>
2021-02-21 00:18:31 -0500

    Created route to add transaction

--- points-web-service/app.py
+++ points-web-service/app.py
@@ -2,20 +2,27 @@ from chalice import Chalice
 
 app = Chalice(app_name='points-web-service')
 app.debug = True
-data_store = {}
 
-@app.route('/')
-def index():
+data_store = []
+
+
+@app.route('/transaction', methods=['POST'])
+def add_transaction():
+    request_body = app.current_request.json_body
+    data_store.append(request_body)
+    return data_store
+
+
+@app.route('/transactions', methods=['POST'])
+def add_transaction():
     return {'status': 'OK'}
 
-@app.route('/points', methods=['GET'])
-def get_payer_points():
-    return {'status': 'OK'}
 
 @app.route('/points', methods=['POST'])
 def spend_points():
     return {'status': 'OK'}
 
-@app.route('/transaction', methods=['POST'])
-def add_transaction():
+
+@app.route('/points', methods=['GET'])
+def get_payer_points():
     return {'status': 'OK'}
