e58c0ef2f8d3f8ad57cecf7d79fc72fbcdc7c1f2
sapinspys <garri009@fiu.edu>
2021-02-21 00:13:29 -0500

    Created route scaffolding

--- points-web-service/app.py
+++ points-web-service/app.py
@@ -1,8 +1,21 @@
 from chalice import Chalice
 
 app = Chalice(app_name='points-web-service')
-
+app.debug = True
+data_store = {}
 
 @app.route('/')
 def index():
+    return {'status': 'OK'}
+
+@app.route('/points', methods=['GET'])
+def get_payer_points():
+    return {'status': 'OK'}
+
+@app.route('/points', methods=['POST'])
+def spend_points():
+    return {'status': 'OK'}
+
+@app.route('/transaction', methods=['POST'])
+def add_transaction():
     return {'status': 'OK'}
